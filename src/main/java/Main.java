935e3b4c0b2248e1aca813faa7c56d7522298574
Artem Oleynikov <allacrit@yandex.ru>
2024-01-15 22:24:29 +0300

    end

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        final RedBlackTree tree = new RedBlackTree();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введите число:");
+            tree.add(scanner.nextInt());
+            System.out.println("Ок!");
+        }
     }
 }
